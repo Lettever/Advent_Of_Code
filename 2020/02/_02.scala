e55d5bc5cceda4f714396a48f6428bef76ef06f9
Lettever <61094520+Lettever@users.noreply.github.com>
2023-10-21 00:51:51 +0100

    Add files via upload

---
+++ 2020/02/_02.scala
@@ -0,0 +1,16 @@
+@main def main() =
+    println("part 1: " + solve(part1))
+    println("part 2: " + solve(part2))
+    
+def solve(func : (String, Char, Int, Int) => Boolean) : Int =
+    io.Source.fromFile("input.txt").getLines().toArray.count(
+    _ match
+        case s"$num1-$num2 $letter: $str" =>
+            func(str, letter(0), num1.toInt, num2.toInt)
+    )
+    
+
+def part1(str : String, ch : Char, min : Int, max : Int) : Boolean =
+    min <= str.count(_ == ch) && str.count(_ == ch) <= max
+def part2(str : String, ch : Char, index1 : Int, index2 : Int) : Boolean =
+    (str(index1 - 1) == ch) ^ (str(index2 - 1) == ch)
---
+++ 2020/12/_12.scala
@@ -0,0 +1,61 @@
+class Position(var x : Int, var y : Int):
+    def +(pos2 : Position) = Position(x + pos2.x, y + pos2.y) 
+    def *(num : Int) = Position(x * num, y * num)
+    def -(pos2 : Position) = this + (pos2 * (-1))
+    def sum() = x + y
+    def abs() = Position(x.abs, y.abs)
+
+class Ferry(var pos : Position, var dir : Char):
+    override def toString(): String = f"${pos.x} ${pos.y} $dir"
+    def move(str : String) =
+        val length = str.drop(1).toInt
+        pos += {
+                str(0) match
+                case 'N' => Position(0, length)
+                case 'E' => Position(length, 0)
+                case 'S' => Position(0, -length)
+                case 'W' => Position(-length, 0)
+                case 'F' => dir match
+                    case 'N' => Position(0, length)
+                    case 'E' => Position(length, 0)
+                    case 'S' => Position(0, -length)
+                    case 'W' => Position(-length, 0)
+            }            
+ 
+    def rotate(str : String) = 
+        val compass = Array('N', 'E', 'S', 'W')
+        var a = compass.indexOf(dir) + {
+            str(0) match
+                case 'R' => str.drop(1).toInt / 90
+                case 'L' => -str.drop(1).toInt / 90
+        }
+        while(a < 0) a += 4
+        while(a >= 4) a -= 4
+        dir = compass(a)
+
+@main def AoC() =
+    println("part1 " + part1())
+    println("part2 " + part2())
+
+def part1() : Int =
+    val ferry = Ferry(Position(0, 0), 'E')    
+    for line <- io.Source.fromFile("input.txt").getLines() do
+        line(0) match
+            case 'N' | 'S' | 'E' | 'W' | 'F' => ferry.move(line)
+            case 'L' | 'R' => ferry.rotate(line)
+    manhattan_distance(ferry.pos, Position(0, 0))
+
+def part2() : Int =
+    val waypoint = Ferry(Position(10, 1), '\u0000')    
+    val ferry = Ferry(Position(0, 0), 'E')    
+    for line <- io.Source.fromFile("input.txt").getLines() do
+        line(0) match
+            case 'N' | 'E' | 'S' | 'W' => waypoint.move(line)
+            case 'L' | 'R' => println("TODO: Rotation")
+            case 'F' => println("TODO: Forward")
+    manhattan_distance(ferry.pos, Position(0, 0))
+
+extension (num : Int)
+    def in_range(min : Int, max : Int) = min <= num && num <= max
+
+def manhattan_distance(pos1 : Position, pos2 : Position) : Int = (pos1 - pos2).abs().sum()
