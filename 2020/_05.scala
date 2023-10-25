f73dc49d3b61a22684080a53f195a72b365d6b83
Lettever <61094520+Lettever@users.noreply.github.com>
2023-10-25 18:57:44 +0100

    Update _05.scala

--- 2020/_05.scala
+++ 2020/_05.scala
@@ -1,5 +1,3 @@
-import scala.language.implicitConversions
-
 @main def AoC() = 
     println("part1 " + part1())
     println("part2 " + part2())
@@ -15,4 +13,4 @@ def row(str : String) : Int = str.foldLeft(0)((acc, ch) => acc * 2 + (ch == 'B')
 def column(str: String) : Int = 7 - str.foldLeft(0)((acc, ch) => acc * 2 + (ch == 'L'))
 def seatId(str : String) : Int = row(str slice(0, 7)) * 8 + column(str slice(7, 10))
 
-implicit def conv(b : Boolean) : Int = if (b) 1 else 0
+extension (b : Boolean) def toInt = if b then 1 else 0
