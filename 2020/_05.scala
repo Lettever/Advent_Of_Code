d1cd72bb2b69d7863b0635fb5d554ff40fdf5541
Lettever <61094520+Lettever@users.noreply.github.com>
2023-10-25 18:57:56 +0100

    Update _05.scala

--- 2020/_05.scala
+++ 2020/_05.scala
@@ -9,8 +9,8 @@ def part2() : Int =
     .map(x => seatId(x)).toArray.sortInPlace()
     seats.zip(seats.tail).find {case (x, y) => y - x == 2}.get(0) + 1
 
-def row(str : String) : Int = str.foldLeft(0)((acc, ch) => acc * 2 + (ch == 'B'))
-def column(str: String) : Int = 7 - str.foldLeft(0)((acc, ch) => acc * 2 + (ch == 'L'))
+def row(str : String) : Int = str.foldLeft(0)((acc, ch) => acc * 2 + (ch == 'B').toInt)
+def column(str: String) : Int = 7 - str.foldLeft(0)((acc, ch) => acc * 2 + (ch == 'L').toInt)
 def seatId(str : String) : Int = row(str slice(0, 7)) * 8 + column(str slice(7, 10))
 
 extension (b : Boolean) def toInt = if b then 1 else 0
