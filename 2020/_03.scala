83f3dc393260a0e5cf9e0cef50b01f5b24c9d471
Lettever <61094520+Lettever@users.noreply.github.com>
2023-10-25 18:54:57 +0100

    Add files via upload

--- 2020/_03.scala
+++ 2020/_03.scala
@@ -1,31 +1,19 @@
-//go to the right 3 and down one until you go off screen
-//going off screen to the right loops you to the back 
-
 val TREE = '#'
 
 @main def AoC() = 
     println("part1 " + part1())
     println("part2 " + part2())
 
-def part1() : Int = io.Source.fromFile("input.txt").getLines.toArray.tree(3, 1)
+def part1() : Long = io.Source.fromFile("input.txt").getLines.toArray.tree(3, 1)
 
 def part2() : Long =
     val map = io.Source.fromFile("input.txt").getLines.toArray
-    map.tree(1, 1).toLong *
-    map.tree(3, 1).toLong *
-    map.tree(5, 1).toLong *
-    map.tree(7, 1).toLong *
-    map.tree(1, 2).toLong
+    Array((1, 1), (3, 1), (5, 1), (7, 1), (1, 2)).
+    map((x_vel, y_vel) => map.tree(x_vel, y_vel)).product
 
-
-extension (map : Array[String])
-    def tree(x_vel : Int, y_vel : Int) : Int =
-        var x = 0
-        var y = 0
-        var trees = 0
-        while y < map.length do
-            if map(y)(x) == TREE then trees += 1
-            x = (x + x_vel) % map(0).length
-            y += y_vel
-        trees
-
+extension (map : Array[String]) def tree(x_vel : Int, y_vel : Int) : Long =
+    (0 until map.length by y_vel).zipWithIndex.foldLeft(0) {
+    case (trees, (y, index)) =>
+        val x = (index * x_vel) % map(0).length
+        if map(y)(x) == TREE then trees + 1 else trees
+    }.toLong
