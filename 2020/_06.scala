c8364baacdd1c06b5a6de0d45392b83081fb1184
Lettever <61094520+Lettever@users.noreply.github.com>
2023-10-25 18:58:43 +0100

    Add files via upload

--- 2020/_06.scala
+++ 2020/_06.scala
@@ -1,30 +1,23 @@
 @main def AoC() = 
     println("part1 " + part1())
     println("part2 " + part2())
-    //val str = "hello"
-    //val countsMap = str.distinct.toCharArray.map(x => (x, str.count(_ == x)))
-    //counts the character in a string
     
 def part1() : Int =
-    var sum = 0
-    var str = ""
-    for line <- io.Source.fromFile("input.txt").getLines() do
-        if line == "" then
-            sum += str.distinct.length
-            str = ""
-        else
-            str += line
-    sum
+    io.Source.fromFile("input.txt").getLines().foldLeft(0, "") {
+        case ((total, string), line) =>
+            if line.isEmpty() then (total + string.distinct.length(), "")
+            else (total, string + line)
+    }(0)
 
 def part2() : Int =
-    var sum = 0
-    var temp : Array[String] = Array()
-    for line <- io.Source.fromFile("input.txt").getLines() do
-        if line == "" then
-            val flattened = temp.mkString
-            val countsMap = flattened.distinct.toCharArray.map(x => (x, flattened.count(_ == x)))
-            sum += countsMap.count((_, x) => x % temp.length == 0)
-            temp = Array()
-        else
-            temp :+= line
-    sum
+    io.Source.fromFile("input.txt").getLines().foldLeft(0, Array.empty[String]) {
+        case ((sum, array), line) =>
+            if line.isEmpty() then 
+                val flattened = array.mkString
+                val charsCount = flattened.distinct.toCharArray.map(x => (x, flattened.count(_ == x)))
+                (sum + charsCount.count((_, x) => x % array.length == 0), Array())
+            else (sum, array :+ line)
+    }(0)
+
+    //val charsCount = string.distinct.toCharArray.map(x => (x, string.count(_ == x)))
+    //counts the frequency of all characters in a string
