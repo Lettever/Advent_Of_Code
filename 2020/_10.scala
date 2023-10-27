3b0e567ff9a0586651408bd1842f2aba995c872d
Lettever <61094520+Lettever@users.noreply.github.com>
2023-10-27 17:53:37 +0100

    Add files via upload

--- 2020/_10.scala
+++ 2020/_10.scala
@@ -3,15 +3,21 @@
     println("part2 " + part2())
 
 def part1() : Int =
-    val numbers = Array(0) ++ io.Source.fromFile("input.txt").getLines().map(_.toInt).toArray.sorted
-    val difference = numbers.zip(numbers.tail ++ Array(numbers.max + 3)).map((x, y) => y - x)
+    val numbers = {
+        val temp = Array(0) ++ io.Source.fromFile("input.txt").getLines().map(_.toInt).toArray.sorted
+        temp :+ temp.max + 3
+    }
+    val difference = numbers.zip(numbers.tail).map((x, y) => y - x)
     difference.count(_ == 1) * difference.count(_ == 3)
 
 def part2() : Long =
-    val numbers = Array(0) ++ io.Source.fromFile("input.txt").getLines().map(_.toInt).toArray.sorted
-    val array = Array.fill(numbers.max + 4) { 0L }
-    for i <- numbers ++ Array(numbers.max + 3) do array(i) = 1L
-    val range = (0 to numbers.max).reverse
+    val numbers = {
+        val temp = Array(0) ++ io.Source.fromFile("input.txt").getLines().map(_.toInt).toArray.sorted
+        temp :+ temp.max + 3
+    }
+    val cumSum = Array.fill(numbers.max + 1) { 0L }
+    for i <- numbers do cumSum(i) = 1L
+    val range = (0 until numbers.max).reverse
     for i <- range do
-        if array(i) != 0 then array(i) = array(i + 1) + array(i + 2) + array(i + 3)
-    array(0)
+        if cumSum(i) != 0 then cumSum(i) = cumSum(i + 1) + cumSum(i + 2) + cumSum(i + 3)
+    cumSum(0)
