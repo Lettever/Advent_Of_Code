633f8ba9b9d1f0d249d8f8141711f3f70599dd08
Lettever <61094520+Lettever@users.noreply.github.com>
2023-12-09 06:10:53 +0000

    Add files via upload

---
+++ 2023/_09.d
@@ -0,0 +1,45 @@
+import std;
+
+void main()
+{
+    writeln("part1 ", solve(1));
+    writeln("part2 ", solve(2));
+}
+
+auto solve(int part)
+{
+    return lines("file.txt").map!(x => x.split(" ").to!(int[])).array
+        .map!(x => next_number(x, part)).sum;
+}
+long next_number(int[] arr, int part)
+{
+    auto arrs = [arr];
+    while(!all_zeros(arrs[$ - 1]))
+    {
+        auto curr_arr = arrs[$ - 1];
+        int[] temp;
+        for(int i = 1; i < curr_arr.length; i++)
+            temp ~= curr_arr[i] - curr_arr[i - 1];
+        arrs ~= temp;
+    }
+    if(part == 1)
+        return arrs.map!(x => x[$ - 1]).sum;
+    auto d = arrs.map!(x => x[0]).array;
+    foreach(i, ref v; d)
+        v *= pow(-1, i);
+    return d.sum;
+}
+bool all_zeros(int[] arr)
+{
+    foreach(n; arr)
+        if(n != 0)
+            return false;
+    return true;
+}
+auto lines(string file)
+{
+    File handle = File(file, "r");
+    scope(exit)
+        handle.close;
+    return handle.byLine.map!(x => x.to!string).array;
+}
