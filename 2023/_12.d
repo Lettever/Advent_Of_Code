9d1cf087e679a3a5afe3584b79ee8046aaefd70e
Lettever <61094520+Lettever@users.noreply.github.com>
2023-12-12 11:35:09 +0000

    Add files via upload

--- 2023/_12.d
+++ 2023/_12.d
@@ -2,67 +2,42 @@ import std;
 
 void main()
 {
-    writeln("part1 ", part1);
-    writeln("part2 ", part2);
+    writeln("part1 ", solve(1));
+    writeln("part2 ", solve(2));
 }
-auto part1()
+long solve(int part)
 {
-    long res = 0;
-	foreach(line; lines("file.txt").map!(x => parse(x)))
-    {
-		auto s = line[0] ~ '.';
-		auto a = line[1];
-		auto n = s.length;
-		auto k = a.length;
-		a ~= n + 1;
-
-		auto f = new long [][][](n + 1, k + 2, n + 2);
-		f[0][0][0] = 1;
-		foreach (i; 0 .. n)
-			foreach (j; 0 .. k + 1)
-				foreach (p; 0 .. n + 1)
-                {
-					auto cur = f[i][j][p];
-					if (!cur)
-						continue;
-					if (s[i] == '.' || s[i] == '?')
-						if (p == 0 || p == a[j - 1])
-							f[i + 1][j][0] += cur;
-					if (s[i] == '#' || s[i] == '?')
-						f[i + 1][j + !p][p + 1] += cur;
-				}
-		res += f[n][k][0];
-	}
-    return res;
+    auto parsed = lines("file.txt").map!(x => parse(x));
+    return reduce!((a, b) => a + calculate(b[0], b[1], part))(0L, parsed);
 }
-long part2()
+long calculate(string str, int[] arr, int part)
 {
-    long res = 0;
-	foreach(line; lines("file.txt").map!(x => parse(x)))
+    if(part == 2)
     {
-		auto s = line[0].repeat(5).join('?') ~ '.';
-		auto a = line[1].repeat(5).join;
-		auto n = s.length;
-		auto k = a.length;
-		a ~= n + 1;
+        str = str.repeat(5).join('?');
+        arr = arr.repeat(5).join;
+    }
+    str ~= '.';
+    auto n = str.length;
+    auto k = arr.length;
+    arr ~= n + 1;
 
-		auto f = new long [][][](n + 1, k + 2, n + 2);
-		f[0][0][0] = 1;
-		foreach (i; 0 .. n)
-			foreach (j; 0 .. k + 1)
-				foreach (p; 0 .. n + 1) {
-					auto cur = f[i][j][p];
-					if (!cur)
-						continue;
-					if (s[i] == '.' || s[i] == '?')
-						if (p == 0 || p == a[j - 1])
-							f[i + 1][j][0] += cur;
-					if (s[i] == '#' || s[i] == '?')
-						f[i + 1][j + !p][p + 1] += cur;
-				}
-		res += f[n][k][0];
-	}
-    return res;
+    auto f = new long [][][](n + 1, k + 2, n + 2);
+    f[0][0][0] = 1;
+    foreach (i; 0 .. n)
+        foreach (j; 0 .. k + 1)
+            foreach (p; 0 .. n + 1)
+            {
+                auto cur = f[i][j][p];
+                if (!cur)
+                    continue;
+                if (str[i] == '.' || str[i] == '?')
+                    if (p == 0 || p == arr[j - 1])
+                        f[i + 1][j][0] += cur;
+                if (str[i] == '#' || str[i] == '?')
+                    f[i + 1][j + !p][p + 1] += cur;
+            }
+    return f[n][k][0];
 }
 Tuple!(string, int[]) parse(string str)
 {
@@ -76,11 +51,3 @@ auto lines(string file)
         handle.close;
     return handle.byLine.map!(x => x.to!string).array;
 }
-/*
-    ???.### 1,1,3 - 1 arrangement = C(2, 2)
-    .??..??...?##. 1,1,3 - 4 arrangements = C(2, 1) + C(2, 1)
-    ?#?#?#?#?#?#?#? 1,3,1,6 - 1 arrangement 
-    ????.#...#... 4,1,1 - 1 arrangement = C(4, 4)
-    ????.######..#####. 1,6,5 - 4 arrangements = C(4, 1)
-    ?###???????? 3,2,1 - 10 arrangements = C(4, 2) + C(4, 1)
-*/
