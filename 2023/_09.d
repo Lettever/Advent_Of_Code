b12824cf0691719d9c24d2af078c4a0e30d7db7f
Lettever <61094520+Lettever@users.noreply.github.com>
2023-12-31 18:00:53 +0000

    Update _09.d

--- 2023/_09.d
+++ 2023/_09.d
@@ -12,25 +12,12 @@ auto solve(int part)
 }
 long next_number(int[] arr, int part)
 {
-    if(part == 2)
-        arr = arr.reverse;
-    auto arrs = [arr];
-    while(!all_zeros(arrs[$ - 1]))
-    {
-        auto curr_arr = arrs[$ - 1];
-        int[] temp;
-        for(int i = 1; i < curr_arr.length; i++)
-            temp ~= curr_arr[i] - curr_arr[i - 1];
-        arrs ~= temp;
-    }
-    return arrs.map!(x => x[$ - 1]).sum;
-}
-bool all_zeros(int[] arr)
-{
-    foreach(n; arr)
-        if(n != 0)
-            return false;
-    return true;
+	if(part == 2)
+		arr = arr.reverse;
+	for(int i = arr.length - 1; i >= 1; i--)
+		for(int j = 0; j < i; j++)
+			arr[j] = arr[j + 1] - arr[j];
+	return arr.sum;
 }
 auto lines(string file)
 {
