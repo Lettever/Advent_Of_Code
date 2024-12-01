a7e62e3cea2c750bf1d06f4750349f20bf249669
Lettever <61094520+Lettever@users.noreply.github.com>
2024-12-01 17:49:32 +0000

    Update _01.d

--- 2024/_01.d
+++ 2024/_01.d
@@ -17,11 +17,11 @@ void main() {
 		zip(arr1, arr2)
 		.map!(x => abs(x[0] - x[1]))
 		.sum();
-	writeln(part1 == 1660292);
+	writeln(part1);
 	int[int] count = arr2.fold!((acc, x) {
 		acc[x] += 1;
 		return acc;
 	})(int[int].init);
 	int part2 = arr1.fold!((acc, x) => acc + x * count.get(x, 0))(0);
-	writeln(part2 == 22776016);
+	writeln(part2);
 }
